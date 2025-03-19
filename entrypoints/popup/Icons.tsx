b3f2a5a1c166b591a1b5db270dddb770c4f6481c
Fabrice Armisen <farmisen@gmail.com>
2025-03-19 14:37:31 -0700

    Fix lint errors

--- entrypoints/popup/Icons.tsx
+++ entrypoints/popup/Icons.tsx
@@ -47,8 +47,8 @@ const Icons = () => {
               size="icon"
               className="h-8 w-8"
               onClick={() => {
-                browser.runtime.openOptionsPage();
-                window.close();
+                browser.runtime.openOptionsPage()
+                window.close()
               }}>
               <Settings className="h-4 w-4" />
               <span className="sr-only">Options</span>
